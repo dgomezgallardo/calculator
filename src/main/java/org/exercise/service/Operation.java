287c81511e3ec3218c26dfececa024523eb959fd
Daniel Gomez Desk <dgomezgallardo@gmail.com>
2024-06-27 22:02:00 +0200

    prepare release 1.0.0

---
+++ src/main/java/org/exercise/service/Operation.java
@@ -0,0 +1,17 @@
+package org.exercise.service;
+
+public class Operation {
+    public void addition(double operator1, double operator2){
+        System.out.println("El resultado es: " + (operator1 + operator2));
+    }
+    public void subtraction(double operator1, double operator2){
+        System.out.println("El resultado es: " + (operator1 - operator2));
+    }
+    public void multiplication(double operator1, double operator2){
+        System.out.println("El resultado es: " + (operator1 * operator2));
+    }
+    public void division(double operator1, double operator2){
+        System.out.println("El resultado es: " + (operator1 / operator2));
+    }
+
+}
